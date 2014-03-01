449cafabad9854e3f250b2a19c1f607d837b621e
mik <mbriday@users.noreply.github.com>
2014-03-01 12:06:50 +0000

    PPC example

---
+++ gadl/examples/powerpc/ioStubs/simulatorSource/ioStubs.h
@@ -0,0 +1,47 @@
+
+#ifndef __arm_dbg_IO_STUBS_H__
+#define __arm_dbg_IO_STUBS_H__
+
+#include <map>
+#include <stdio.h> //FILE 
+
+#include "types.h"
+#include "action.h"
+
+class ioStubOpen : public executeAction {
+	std::map<int , FILE *> m_openedFiles;
+public :
+	ioStubOpen() : executeAction() {};
+	virtual ~ioStubOpen() {};
+    virtual bool trigger(core *a);
+	FILE *fileFor(int fd);
+	std::string functionName() {return std::string("_open_r");};
+};
+
+class ioStubRead : public executeAction {
+	ioStubOpen *m_iso;
+public :
+	ioStubRead(ioStubOpen *iso) : executeAction(), m_iso(iso) {};
+	virtual ~ioStubRead() {};
+    virtual bool trigger(core *a);
+	std::string functionName() {return std::string("_read_r");};
+};
+
+class ioStubWrite : public executeAction {
+	ioStubOpen *m_iso;
+public :
+	ioStubWrite(ioStubOpen *iso) : executeAction(), m_iso(iso) {};
+	virtual ~ioStubWrite() {};
+    virtual bool trigger(core *a);
+	std::string functionName() {return std::string("_writeToFile");};
+};
+
+class ioStubPrintStdout : public executeAction {
+	ioStubOpen *m_iso;
+public :
+	ioStubPrintStdout() : executeAction() {};
+	virtual ~ioStubPrintStdout() {};
+    virtual bool trigger(core *a);
+	std::string functionName() {return std::string("writechar");};
+};
+#endif
