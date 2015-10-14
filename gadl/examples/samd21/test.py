73f517e4672d817ce862a9685a1e8e5dd46f5440
mik <mbriday@users.noreply.github.com>
2015-10-14 19:57:33 +0000

    samd21 processor. Base files.

---
+++ gadl/examples/samd21/test.py
@@ -0,0 +1,19 @@
+#!/opt/local/bin/python2.7
+import sys
+sys.path.append("./samd21")
+try:
+    import samd21
+except ImportError:
+    print "simulator lib not generated"
+    print "first run gadl:"
+    print "$gadl ./samd21.cpu"
+    print "Then go to the new samd21/ dir and build the python lib"
+    print "$cd samd21"
+    print "$make python"
+    sys.exit()
+    
+sam = samd21.cpu()
+core = sam.getCore(0)
+core.readCodeFile('./testIO.elf')
+print core.disassemble(0xb9e,1,True);
+core.printRegs()
