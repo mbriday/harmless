08e4210bd83ff2e8ea5099e0c6b526abaf7f0c09
mik <mbriday@users.noreply.github.com>
2015-11-27 14:45:33 +0000

    [cortex-M] format16 bits done (not tested)

---
+++ gadl/examples/samd21/test/deasm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Usage : ./deasm.py <code_file> <number of instruction to disassemble>
+# Disassemble the <number> first instruction of the given file.
+# this file is called by test_syntax.sh
+import sys
+
+# Looks in powerpc directory to find the processor arch
+sys.path.append("../samd21")
+from samd21 import *
+
+uc=cpu()
+sam=uc.getCore(0)
+sam.readCodeFile(sys.argv[1])
+#sam.setProgramCounter(0x0)
+
+for i in range(int(sys.argv[2])/2):
+    print sam.disassemble(i*2, 2, False)
+
