837067eaf0b0b8b596e863f75febbf19c65c33c1
Mikael BRIDAY <mikael.briday@univ-nantes.fr>
2018-06-05 15:24:48 +0200

    [cortexM] behavior of add.w. Not tested.

--- gadl/examples/samd21/test/behavior/behaviorTest.py
+++ gadl/examples/samd21/test/behavior/behaviorTest.py
@@ -754,5 +754,6 @@ if __name__ == '__main__':
             nbInstructions += 1
         else: # assembly only
             subprocess.call(["arm-none-eabi-objdump", "-d", filename+'.elf'],)
-    debugStr(args,0,str(nbTestsDone)+' tests done for '+str(nbInstructions)+' instructions in {t:.2f}s, ok at {p:.1f}%.\n'.format(t=(time.clock()-timeStart),p=(100*nbTestsDone)/nbTests))
+    if nbTests != 0:
+        debugStr(args,0,str(nbTestsDone)+' tests done for '+str(nbInstructions)+' instructions in {t:.2f}s, ok at {p:.1f}%.\n'.format(t=(time.clock()-timeStart),p=(100*nbTestsDone)/nbTests))
 
